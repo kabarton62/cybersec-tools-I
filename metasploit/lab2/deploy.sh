6304cfdd4a868da755fe4ee2c181ebbea053fea1
Kevin Barton <kbarton@tamusa.edu>
2022-11-13 20:10:39 -0700

    Update deploy.sh

--- metasploit/lab2/deploy.sh
+++ metasploit/lab2/deploy.sh
@@ -1,1 +1,24 @@
+#!/bin/bash
+docker='/usr/bin/sudo /usr/bin/docker'
+f=docker-compose.yml
+web='tleemcjr/metasploitable2:latest'
+wname='metasploitable'
+webip='172.19.2.3'
 
+printf "\nBe patient. Stopping and deleting metasploitable.\n\n"
+
+# Deploy metasploitable2
+$docker stop metasploitable && $docker rm metasploitable
+$docker run --name $wname -it -d tleemcjr/metasploitable2:latest sh -c "/bin/services.sh && bash"
+
+printf "\nCreating a new metasploitable.\n\n"
+
+# create a new docker network and attach metasploitable2
+$docker network rm hidden
+$docker network create -d bridge hidden --subnet 192.168.100.0/24
+$docker network connect --ip 192.168.100.15 hidden metasploitable
+
+printf "\nChanging configuration for tomcat.\n\n"
+# configure tomcat
+$docker cp ./tomcat/server.xml $wname:/etc/tomcat5.5/server.xml
+$docker exec -it $wname service tomcat5.5 restart 
