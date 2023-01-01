06500a85ec39f143ab6c6c4071563ff4113a16ff
Kevin Barton <kbarton@tamusa.edu>
2023-01-01 15:43:23 -0700

    Update deploy.sh

--- python/deploy.sh
+++ python/deploy.sh
@@ -1,5 +1,6 @@
 /usr/bin/sudo /usr/bin/docker build -t python2:latest .
 
 /usr/bin/sudo /usr/bin/docker run -itd -v scripts:/root/ --name python --hostname python python2:latest
-/usr/bin/sudo /usr/bin/docker exec -it python apt update && apt install nano
+/usr/bin/sudo /usr/bin/docker exec -it python apt update
+/usr/bin/sudo /usr/bin/docker exec -it pythonapt install nano
 /usr/bin/sudo /usr/bin/docker exec -it python pip install sh
