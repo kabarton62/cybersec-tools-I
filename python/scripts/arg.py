c276445f09747204556739ab85fb4995c0a2e983
Kevin Barton <kbarton@tamusa.edu>
2023-01-01 14:41:40 -0700

    Update arg.py

--- python/scripts/arg.py
+++ python/scripts/arg.py
@@ -1,9 +1,10 @@
+#-*- coding: utf-8 -*-
+
 import sys
 
 if len(sys.argv) != 2:
- raise ValueError('Please provide a subnet, i.e. "192.168.1."')
-
-print(f'Script Name is {sys.argv[0]}')
+  raise ValueError('Please provide a subnet, i.e. "192.168.1."')
+print("Script Name is {}".format(sys.argv[0]))
 
 subnet = sys.argv[1]
-print(f'pinging subnet {subnet}0/24')
+print("pinging subnet {}0/24".format(subnet))
