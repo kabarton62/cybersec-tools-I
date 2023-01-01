249c0bdfbaa5abc39119a3802adb1c171702707e
Kevin Barton <kbarton@tamusa.edu>
2023-01-01 15:00:18 -0700

    Update deploy.sh

--- python/deploy.sh
+++ python/deploy.sh
@@ -1,3 +1,3 @@
 /usr/bin/sudo /usr/bin/docker build -t python2:latest .
 
-/usr/bin/sudo /usr/bin/docker run -itd --name python --hostname python python2:latest
+/usr/bin/sudo /usr/bin/docker run -itd --name python2 --hostname python2 python2:latest
